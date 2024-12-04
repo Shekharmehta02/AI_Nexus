52889800de2fa5299bbeadde78a4975c8081cd0b
Shekhar_Mehta <Shekharmehta643@gmail.com>
2024-12-05 02:01:22 +0530

    V11.2

---
+++ Addition.py
@@ -0,0 +1,5 @@
+def Addition(x, y):
+    z = x+y
+    print("Addition of {} & {} :".format(x,y), z)
+
+Addition(2, 3)
--- Path_Cal.py
+++ Path_Cal.py
@@ -2,3 +2,6 @@ import os
 
 def file_collect():
     return os.getcwd()
+
+def csv_file_Collect():
+    pass
